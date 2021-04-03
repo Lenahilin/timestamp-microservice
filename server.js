38a201897dfd6db097197ba2fa47bf473e142d42
Elena Agapova <elena.agapova@rohlik.cz>
2021-04-03 21:59:07 +0200

    headers parser endpoint done

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ app.use(morgan('combined'))
 app.use(express.static('public'));
 
 // homepage with usage
-app.get('/', function (req, res) {
+// TODO: move all api's usage to the homepage
+app.get('/',  (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// TODO: move all routes to a separate router https://expressjs.com/en/guide/routing.html
 app.get('/api/timestamp/:timestamp?', (req, res) => {
   if (req.params.timestamp === undefined) {
     var timestamp = new Date();
@@ -23,11 +25,17 @@ app.get('/api/timestamp/:timestamp?', (req, res) => {
   }
 
   if (timestamp == 'Invalid Date') {
-    res.json({error : 'Invalid Date'});
+    res.json({error: 'Invalid Date'});
   } else {
     res.json({unix: timestamp[Symbol.toPrimitive]('number'), utc: timestamp.toUTCString()});
   }
 })
+
+app.get('/api/whoami', (req, res) => {
+  res.json({ipaddress: req.ip, 
+            language: req.headers['accept-language'], 
+            software: req.headers['user-agent']});
+});
 
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
